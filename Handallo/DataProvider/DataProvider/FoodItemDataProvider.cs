edcb553348a7094c2805c74ad8f0ef0621ca88a8
Dulanga Heshan <164056m@uom.lk>
2018-11-07 22:22:28 +0530

    fixed connection string and add foodIem freatures

---
+++ Handallo/DataProvider/DataProvider/FoodItemDataProvider.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Handallo.DataProvider.DataProvider
+{
+    public class FoodItemDataProvider
+    {
+        private readonly String connectionString;
+        private String checkExist;
+
+        public FoodItemDataProvider()
+        {
+            connectionString = "Server=DESKTOP-ALMQ9QA\\SQLEXPRESS;Database=handallo;Trusted_Connection=True;MultipleActiveResultSets=true";
+            //connectionString = "Server=tcp:handallo.database.windows.net;Database=handallo;User ID=Handallo.336699;Password=16xand99x.;Trusted_Connection=false;MultipleActiveResultSets=true";
+            //// connectionString = "Server=tcp: handallo.database.windows.net,1433; Initial Catalog = Handallo;Database=handallo; User ID = Handallo.336699; Password = 16xand99x.Trusted_Connection=True;MultipleActiveResultSets=true";
+        }
+
+        //public IDbConnection Connection
+        //{
+        //    get
+        //    {
+        //        return new SqlConnection(connectionString);
+        //    }
+        //}
+
+        public SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(connectionString);
+            }
+        }
+
+        public String AddFoodItems()
+        {
+
+            return "ok";
+        }
+
+    }
+}
---
+++ Handallo/Models/FoodItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Handallo.Models
+{
+    public class FoodItem
+    {
+        public int ShopId { get; set; }
+        public int TypeId { get; set; }
+        public int ItemId { get; set; }
+        public Boolean IsDinner { get; set; }
+        public Boolean IsLunch { get; set; }
+        public Boolean IsBreakFast { get; set; }
+
+        public String  Path { get; set; }
+        public String  Url { get; set; }
+
+        public Boolean Avaliability { get; set; }
+
+    }
+}
